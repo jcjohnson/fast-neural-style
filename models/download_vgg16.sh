d66e4ccadc7060ccb792517c11851a260af0e66d
Jirka Kremser <jkremser@redhat.com>
2017-12-22 23:16:30 +0100

    Fixes #143: Follow 301 redirects in the bash script that downloads the models

--- models/download_vgg16.sh
+++ models/download_vgg16.sh
@@ -1,3 +1,3 @@
 cd models
-curl -O "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/vgg16.t7"
+curl -LO "http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/vgg16.t7"
 cd ../
